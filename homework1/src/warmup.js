fa1b1384f99fe4d19665eeb5ef034a290ea77d4a
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2019-09-13 18:34:02 -0700

    Update warmup.js
    Change solved

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,1 +1,30 @@
+//HOMEWORK 1, SEBASTIAN GRASSO
+
+function change(cents){
+  if (cents < 0){
+    throw new RangeError("RangeError: amount cannot be negative");
+  }
+
+  const coins = [];
+  const quarter = Math.floor(coins/25);
+  coins = coins % 25;
+  const dime = Math.floor(coins/10);
+  coins = coins % 10;
+  const nickel = Math.floor(coins/5);
+  coins = coins % 5;
+  const penny = coins;
+}
+
+module.exports = {
+    change,
+    stripQuotes,
+    scramble,
+    powers,
+    powersGenerator,
+    say,
+    interleave,
+    cylinder,
+    makeCryptoFunctions,
+    randomName
+}
 
