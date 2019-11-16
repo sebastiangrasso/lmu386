84d49b57439a928d976ae1da8dab7dd19825bf5f
sgrasso <sebastian.grasso25@gmail.com>
2019-11-15 22:32:26 -0800

    working, passes unit tests

--- homework4/StreamPractice.java
+++ homework4/StreamPractice.java
@@ -1,14 +1,28 @@
-// IMPORTS GO HERE
+//Homework 4: Sebastian Grasso
+
+import java.util.stream.Collectors.*;
+import java.util.Optional;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.TreeMap;
+import java.io.BufferedReader;
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+
+
 
 public class StreamPractice {
 
     static Pattern nonWord = Pattern.compile("[^\\p{L}']+");
 
     public static Map<Integer, Long> wordCountByLength(BufferedReader reader) {
-        // Return a tree map whose keys are word lengths
-        // and corresponding values are the number of words
-        // in the reader with that length. For "length" of a
-        // string, just use Java’s length method on strings.
+        return reader
+            .lines()
+            .flatMap(nonWord::splitAsStream)
+            .filter(s -> s.length() > 0)
+            //Collect Stream into TreeMap
+            .collect(groupingBy(String::length, TreeMap::new, counting()));
     }
 
     public static class Batter {
@@ -28,9 +42,11 @@ public class StreamPractice {
     }
 
     public static Map<String, Optional<Batter>> bestBatterByTeam(BufferedReader reader) {
-        // Return a map that records, for each team, the batter with
-        // the highest average over all batters that have at least
-        // 100 at-bats. If a team has no batters with at least 100 at
-        // bats, the team will not appear in the map.
-    }
+        return reader
+            .lines()
+            .map(player -> new Batter(player))
+            .filter(player -> (player.atBats >= 100))
+            //Collect Players with highest batting average from each team
+            .collect(groupingBy(player -> player.team, maxBy(comparing(player->player.average))));
+        }
 }
